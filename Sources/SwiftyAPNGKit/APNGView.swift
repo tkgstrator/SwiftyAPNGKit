958738a8e1431e181d9f5f7a8f4d727e33386172
tkgstrator <nasawake.am@gmail.com>
2021-05-23 18:06:07 +0900

    - なんか修正したかも

--- Sources/SwiftyAPNGKit/APNGView.swift
+++ Sources/SwiftyAPNGKit/APNGView.swift
@@ -2,11 +2,10 @@ import Foundation
 import SwiftUI
 import APNGKit
 
-public struct APNGView: UIViewControllerRepresentable {
+public struct APNGView: UIViewRepresentable {
 
     let image: APNGImage?
-    var tmp: Image? = nil
-    
+
     public init(url: URL) {
         self.image = APNGImage(url: url)
     }
@@ -14,33 +13,24 @@ public struct APNGView: UIViewControllerRepresentable {
     public init(url urlString: String) {
         self.init(url: URL(string: urlString)!)
     }
-    
-//    public init(data: Data) {
-//        self.image = APNGImage(data: data)
-//    }
-//
-//    public init(contentsOfFile: String) {
-//        self.image = APNGImage(contentsOfFile: contentsOfFile)
-//    }
-    
+
     public init(named: String) {
         self.image = APNGImage(named: named)
     }
 
-    public func makeUIViewController(context: Context) -> UIViewController {
-        let viewController = UIViewController()
-        guard let image = self.image else { return viewController }
-        let imageView = APNGImageView(image: image)
-        imageView.startAnimating()
-        viewController.view.addSubview(imageView)
-        return viewController
+    public func updateUIView(_ uiView: UIView, context: Context) {
     }
     
+    public func makeUIView(context: Context) -> UIView {
+        guard let image = self.image else { return UIView() }
+        let imageView = APNGImageView(image: image)
+        imageView.startAnimating()
+        return imageView
+    }
+
     public func makeCoordinator() -> () {
     }
 
-    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-    }
 }
 
 extension APNGImage {
